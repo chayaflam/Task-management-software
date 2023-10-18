f3f2a840dd93d3d3852bcd88fff80fab63ae256f
קולומבוס עדינה טובה <Hand4c26@mbjcomp.org.il>
2023-10-18 14:29:10 +0300

    initial code + Welcome functions

---
+++ Stage0/Program6367.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Stage0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            welcome6367();
+            welcome9589();
+            Console.ReadKey();
+        }
+
+        static partial void welcome9589();
+
+        private static void welcome6367()
+        {
+            Console.WriteLine("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+    }
+}
+
