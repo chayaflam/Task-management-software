30d9ce3f393e19b05ee45740d4491062a0856660
פלם חיה <Hand4c24@mbjcomp.org.il>
2023-12-25 19:06:43 +0200

    המשכנו טיפטיפה בשלב 8

---
+++ BL/BO/Exceptions.cs
@@ -0,0 +1,13 @@
+﻿
+
+namespace BO;
+
+/// <summary>
+/// Id already exists exception
+/// </summary>
+[Serializable] 
+    public class BlAlreadyExistsException : Exception
+    {
+        public BlAlreadyExistsException(string? message) : base(message) { }
+    }
+
---
+++ BL/BlImplementation/EngineerImplementation.cs
@@ -0,0 +1,48 @@
+﻿namespace BlImplementation;
+using BlApi;
+using System;
+using System.Collections.Generic;
+using System.Xml.Linq;
+using System.Xml;
+
+internal class EngineerImplementation : IEngineer
+{
+    private DalApi.IDal _dal =Factory.Get;
+
+    public int Create(BO.Engineer boEngineer)
+    {
+        DO.Engineer doEngineer = new DO.Engineer
+         (boEngineer.Id, boEngineer.Name, boEngineer.Email, boEngineer.Level, boEngineer.Cost);
+        try
+        {
+            int idEngineer = _dal.Engineer.Create(doEngineer);
+            return idEngineer;
+        }
+        catch (DO.DalAlreadyExistsException ex)
+        {
+            throw new BO.BlAlreadyExistsException($"Engineer with ID={boEngineer.Id} already exists", ex);
+        }
+
+
+
+}
+    public void Delete(int id)
+    {
+       
+    }
+
+    public BO.Engineer? Read(int id)
+    {
+      
+    }
+
+    public IEnumerable<BO.Engineer?> ReadAll(Func<BO.Engineer?, bool>? filter = null)
+    {
+        
+    }
+
+    public void Update(BO.Engineer item)
+    {
+       
+    }
+}
