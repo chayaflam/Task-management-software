47cb7a63d37a7ae54cc2095e3380fa59fad47a39
קולומבוס עדינה טובה <Hand4c26@mbjcomp.org.il>
2023-12-27 16:59:56 +0200

    ☺☺

--- BL/BlImplementation/EngineerImplementation.cs
+++ BL/BlImplementation/EngineerImplementation.cs
@@ -8,12 +8,17 @@ using System.Data.Common;
 
 internal class EngineerImplementation : IEngineer
 {
-    private DalApi.IDal _dal =Factory.Get;
+    private DalApi.IDal _dal = Factory.Get;
 
     public int Create(BO.Engineer boEngineer)
     {
+        if (boEngineer.Id <= 0 || boEngineer.Name == "" || boEngineer.Name == null ||
+           boEngineer.Email == "" || boEngineer.Email == null || boEngineer.Cost <= 0)
+            throw new bo();
+
+
         DO.Engineer doEngineer = new DO.Engineer
-         (boEngineer.Id, boEngineer.Name, boEngineer.Email,(DO.EngineerExperience)boEngineer.Level, boEngineer.Cost);
+         (boEngineer.Id, boEngineer.Name, boEngineer.Email, (DO.EngineerExperience?)boEngineer.Level, boEngineer.Cost);
         try
         {
             int idEngineer = _dal.Engineer.Create(doEngineer);
@@ -24,59 +29,86 @@ internal class EngineerImplementation : IEngineer
             // throw new BO.BlAlreadyExistsException($"Engineer with ID={boEngineer.Id} already exists", ex);
             return 0;
         }
+    }
 
-
-
-}
     public void Delete(int id)
     {
-
         DO.Engineer? doEngineer = _dal.Engineer.Read(id);
-        //if (doEngineer == null)
-           // throw new BO.BlDoesNotExistException($"doEngineer with ID={id} does Not exist");
-       // List<Task> tasks = new List<Task>();
+        if (doEngineer == null)
+            throw new BO.BlDoesNotExistException($"Engineer with ID={id} does Not exist");
+
+        List<DO.Task?> tasks = (List<DO.Task?>)_dal.Task.ReadAll();
+        DO.Task? task = tasks.FirstOrDefault(x => x!.EngineerId == id);
+        if (task != null)
+        {
+            throw new BO.BlNotErasable($"Engineer with ID={id}cannot be deleted");
+        }
+        try
+        {
+            _dal.Engineer.Delete(id);
+        }
+        catch (Exception ex)
+        {
+            throw new BO.BlNotErasable($"Engineer with ID={id}cannot be deleted");
+        }
+
     }
 
     public BO.Engineer? Read(int id)
     {
         DO.Engineer? doEngineer = _dal.Engineer.Read(id);
         if (doEngineer == null)
-            //throw new BO.BlDoesNotExistException($"doEngineer with ID={id} does Not exist");
+            throw new BO.BlDoesNotExistException($"Engineer with ID={id} does Not exist");
 
         return new BO.Engineer()
-            {
-                Id = id,
-                Name = doEngineer!.Name,
-                Email = doEngineer!.Email,
-                Level = (BO.EngineerExperience?) doEngineer.Level,
-                Cost=doEngineer.Cost,
-            };
+        {
+            Id = id,
+            Name = doEngineer!.Name!,
+            Email = doEngineer!.Email!,
+            Level = (BO.EngineerExperience)doEngineer.Level!,
+            Cost = (double)doEngineer.Cost!,
+        };
 
     }
- 
+
     public IEnumerable<BO.Engineer?> ReadAll(Func<BO.Engineer?, bool>? filter = null)
     {
         return (from DO.Engineer doEngineer in _dal.Engineer.ReadAll()
                 select new BO.Engineer()
                 {
                     Id = doEngineer.Id,
-                    Name = doEngineer!.Name,
-                    Email = doEngineer!.Email,
-                    Level = (BO.EngineerExperience?)doEngineer.Level,
-                    Cost = doEngineer.Cost,
+                    Name = doEngineer.Name!,
+                    Email = doEngineer.Email!,
+                    Level = (BO.EngineerExperience)doEngineer.Level!,
+                    Cost = (double)doEngineer.Cost!,
                     Task = (BO.TaskInEngineer)(from DO.Task doTask in _dal.Task.ReadAll()
-                            where doTask.EngineerId == doEngineer.Id
-                            select new BO.TaskInEngineer()
-                            {
-                                Id = doTask.Id,
-                                Alias = doTask.Alias
-                            })
+                                               where doTask.EngineerId == doEngineer.Id
+                                               select new BO.TaskInEngineer()
+                                               {
+                                                   Id = doTask.Id,
+                                                   Alias = doTask.Alias
+                                               })
                 }
-                ); 
+                );
     }
 
-    public void Update(BO.Engineer item)
+    public void Update(BO.Engineer boEngineer)
     {
-       
+        if (boEngineer.Id <= 0 || boEngineer.Name == "" ||
+           boEngineer.Email == "" || boEngineer.Cost <= 0)
+            throw new bo();
+            DO.Engineer doEngineer = new DO.Engineer
+             (boEngineer.Id, boEngineer.Name, boEngineer.Email, (DO.EngineerExperience?)boEngineer.Level, boEngineer.Cost);
+        try
+            {
+                _dal.Engineer.Update(doEngineer);
+            }
+            catch (Exception ex)
+            {
+            throw new BO.bl();
+            }
+
+
+
     }
 }
--- BL/BlImplementation/MilestoneImplementation.cs
+++ BL/BlImplementation/MilestoneImplementation.cs
@@ -1,12 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using BlApi;
+using DalApi;
 
-namespace BlImplementation
+namespace BlImplementation;
+
+internal class MilestoneImplementation :IMilestone
 {
-    internal class MilestoneImplementation
+    private DalApi.IDal _dal = Factory.Get;
+    public IEnumerable<BO.Milestone> Create(IEnumerable<BO.Task> tasks)
     {
+        
+    }
+
+    public BO.Milestone? Read(int id)
+    {
+        IEnumerable<DO.Dependency>
+        DO.Task? doMilestone = _dal.Task.Read(id);
+        if (doMilestone!.Milestone == false)
+            throw new BL();
+        
+       
+        if (doTask == null)
+            throw new BO.BlDoesNotExistException($"Engineer with ID={id} does Not exist");
+        
+    }
+
+    public void Update(BO.Milestone item)
+    {
+        
     }
 }
